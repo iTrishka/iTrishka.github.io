db578599e6d0cb3f9d1da99c33c142793d8c295d
iTrishka <snowflak@yandex.ru>
2022-08-02 23:22:11 +0300

    add react-burger page

--- react-burger/src/services/checkResponse.ts
+++
@@ -1,7 +0,0 @@
-
-export default function checkResponse(res:Response) {
-    if (res.ok) {
-        return res.json()
-    }
-    return res.json() 
-}
--- react-burger/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://norma.nomoreparties.space/api/';
-export const WS_URL = 'wss://norma.nomoreparties.space/orders';
-
